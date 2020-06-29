63a72e6b36b853f70ba2bc05b42ac44436a96b7e
Anthony Jimenez <anthonyjim96@gmail.com>
2020-06-29 20:34:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
+const guardClause = (array, message) => if(array.length === 0) message;
+
 const takeANumber = (currentLine, newPerson) => {
   currentLine.push(newPerson);
-  return `Welcome, ${newPerson}. You are number ${currentLine.length} in line.`
+  return `Welcome, ${newPerson}. You are number ${currentLine.length} in line.`;
 }
 
 const nowServing = (arrayOfPeople) => {
+  guardClause(arrayOfPeople, 'There is nobody waiting to be served!');
   const firstElm = arrayOfPeople.shift();
-  return `Currently serving ${firstElm}.`
+  return `Currently serving ${firstElm}.`;
 }
 
 const currentLine = (arrayOfPeople) => {
-  if(arrayOfPeople.length == 0) return `The line is currently empty.`
-   return `The line is currently: ${arrayOfPeople.map((person, index) => (index === 0) ? `${index+1}. ${person}` : ` ${index+1}. ${person}`)}` // whitespace is needed for every element after the first
+   guardClause(arrayOfPeople, `The line is currently empty.`);
+   return `The line is currently: ${arrayOfPeople.map((person, index) => (index === 0) ? `${index+1}. ${person}` : ` ${index+1}. ${person}`)}`; // whitespace is needed for every element after the first
 }
