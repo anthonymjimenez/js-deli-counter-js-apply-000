09bc9ae37f243bb3bcff31d2d8308620609607cd
Anthony Jimenez <anthonyjim96@gmail.com>
2020-06-29 20:26:11 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,14 @@
+const takeANumber = (currentLine, newPerson) => {
+  currentLine.push(newPerson);
+  return `Welcome, ${newPerson}. You are number ${currentLine.length} in line.`
+}
+
+const nowServing = (arrayOfPeople) => {
+  const firstElm = array1.shift();
+  return `Currently serving ${firstElm}`
+}
+
+const currentLine = (arrayOfPeople) => {
+  if(arrayOfPeople.length == 0) return `The line is currently empty`
+   return `The line is currently: ${arrayOfPeople.map((person, index) => ` ${index+ 1}. ${person}`)}`
+}
