b82205e2e28986fe93e51c925ccc2c2b0716410c
Anthony Jimenez <anthonyjim96@gmail.com>
2020-06-29 20:41:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const takeANumber = (currentLine, newPerson) => {
 }
 
 const nowServing = (arrayOfPeople) => {
-  if(checkForEmptyArray(arrayOfPeople)) return 'There is nobody waiting to be served!'
+  if(isEmptyArray(arrayOfPeople)) return 'There is nobody waiting to be served!'
   const firstElm = arrayOfPeople.shift();
   return `Currently serving ${firstElm}.`;
 }
 
 const currentLine = (arrayOfPeople) => {
-   if(checkForEmptyArray(arrayOfPeople)) return `The line is currently empty.`;
+   if(isEmptyArray(arrayOfPeople)) return `The line is currently empty.`;
    return `The line is currently: ${arrayOfPeople.map((person, index) => (index === 0) ? `${index+1}. ${person}` : ` ${index+1}. ${person}`)}`; // whitespace is needed for every element after the first
 }
