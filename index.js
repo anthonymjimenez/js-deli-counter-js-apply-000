64e470dc881cdb9bc28546288babcb44b88ce2b9
Anthony Jimenez <anthonyjim96@gmail.com>
2020-06-29 20:42:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const isEmptyArray = (array) => (Array.isArray(array)) ? (array.length === 0) ? true : false : false
+const isEmptyArray = (array) => (Array.isArray(array)) ? (array.length === 0) ? true : false : false // only passes true when 'array' argument is 1) An array and 2) Equal to Zero in Length
 
 const takeANumber = (currentLine, newPerson) => {
   currentLine.push(newPerson);
